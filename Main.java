fd2ee43ac97aa494a9f2fcd509c95593edbdaba8
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-05 19:39:44 +0530

    Added : code for Welcome message

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Message information = new Message();
+		information.message();
+	}
+
+}
---
+++ Message.java
@@ -0,0 +1,8 @@
+
+public class Message {
+	
+	public void message() {
+		System.out.println("Welcome to Line compersion Problems");
+	}
+
+}
